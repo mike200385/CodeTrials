683914daecf287110f345b885011c5d3de7114e0
mike200385 <mikewilliams76901@gmail.com>
2017-04-22 00:34:33 -0400

    LoopLevel Updates

--- CodeTrials/Assets/Scripts/PersistantData/GlobalController.cs
+++ CodeTrials/Assets/Scripts/PersistantData/GlobalController.cs
@@ -204,8 +204,8 @@ public class GlobalController : MonoBehaviour {
 	public void resetWhenSceneChanged(){
 		thePlayer = FindObjectOfType<PlayerMovement> ();
 		mainCam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
-		scoreText = GameObject.FindGameObjectWithTag ("ScoreText").GetComponent<Text>();
-		scientistText = GameObject.FindGameObjectWithTag ("ScientistText").GetComponent<Text>();
+//		scoreText = GameObject.FindGameObjectWithTag ("ScoreText").GetComponent<Text>();
+//		scientistText = GameObject.FindGameObjectWithTag ("ScientistText").GetComponent<Text>();
 		wordDisplay = GameObject.FindGameObjectWithTag ("JITDisplay").GetComponent<Text>();
 
 		//display that text
